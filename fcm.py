5e4f29dc3dc38eac0f577a9150467ac4bd629e5a
coderdhruv <f20170108@hyderabad.bits-pilani.ac.in>
2021-04-21 13:55:50 +0530

    Added daily fcm

--- fcm.py
+++ fcm.py
@@ -6,13 +6,13 @@ import datetime
 import calendar
 from fcmeans import FCM
 
+
 class FCmeans:
     def __init__(self, monthly_data):
         data_for_clustering = self.preprocess_monthly_data_to_fit_fcmeans_library(
             monthly_data)
         self.fcmeans = FCM(n_clusters=5, random_state=0)
         self.fcmeans.fit(data_for_clustering)
-        
 
     def preprocess_monthly_data_to_fit_fcmeans_library(self, monthly_data):
         list_to_pass_fcmeans_function = []
@@ -32,13 +32,42 @@ class FCmeans:
     # def get_labels_for_each_data_point(self):
     #     return self.fcmeans.predict(monthly_data)
 
+
+class FCmeansDaily:
+    def __init__(self, training_data):
+        data_for_clustering = self.preprocess_monthly_data_to_fit_fcmeans_library(
+            training_data)
+        self.fcmeans = FCM(n_clusters=5, random_state=0)
+        self.fcmeans.fit(data_for_clustering)
+
+    def preprocess_daily_data_to_fit_fcmeans_library(self, training_data):
+        list_to_pass_kmeans_function = []
+        for i in range(0, len(training_data)):
+            temp = []
+            temp.append(training_data[i].upper_shadow_length)
+            temp.append(training_data[i].lower_shadow_length)
+            temp.append(training_data[i].body_length)
+            temp.append(training_data[i].color)
+            list_to_pass_kmeans_function.append(temp)
+        data_for_clustering = np.array(list_to_pass_kmeans_function)
+        return data_for_clustering
+
+    def get_clusters(self):
+        return self.fcmeans.centers
+
+    def get_labels_list(self):
+        X = self.preprocess_monthly_data_to_fit_fcmeans_library(
+            training_data)
+        return self.fcm.predict(X)
+
+    def get_labels_for_each_data_point(self, data_point_index):
+        return self.get_labels_list()[data_point_index]
+
+
 df = investpy.get_stock_historical_data(
     stock='AAPL', country='United States', from_date='01/01/2000', to_date='01/01/2001')
 print(df)
-preprocess_obj = preprocess()
-myobject = datetime.datetime.strptime('2000-01-01', "%Y-%m-%d")
-myobject2 = datetime.datetime(2000, 1, 1, 0, 0)
-monthly_data = preprocess_obj.get_three_monthly_candlestick_data(
-    df, '2000-01-01')
-fcmeans = FCmeans(monthly_data)
+daily_data = preprocess_obj.get_training_data(
+    'AAPL', 'United States', '2000-01-01', '2000-02-01')
+fcmeans = FCmeansDaily(daily_data)
 print(fcmeans.get_clusters())
